866c8cccffea0c890af5a61fe5a5bb6ce021499f
Kulimi <kulimi@kulimi.tw>
2024-03-02 11:31:38 +0800

    fix: change download api from fileId to filepath

--- tts-frontend/src/utils/utils.ts
+++ tts-frontend/src/utils/utils.ts
@@ -1,6 +1,6 @@
 export function downloadFile(fileId: string) {
     const link = document.createElement('a')
-    link.href = `/download/${fileId}`;
+    link.href = `/download/${fileId}.mp3`;
   
     link.download = `${fileId}.mp3`
     document.body.appendChild(link)
